52a021913b221c8455c752914aa9a00c455b153c
nnp <nnp.nikiforuk@gmail.com>
2025-05-06 16:11:01 +0300

    added base config

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Search() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-5xl font-extrabold">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
