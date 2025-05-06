b49e2a268dc1a3ef4febc387015640dfb97ad9f7
nnp <nnp.nikiforuk@gmail.com>
2025-05-06 18:30:13 +0300

    added search input with dropdown and setup background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-export default function Search() {
+import SearchInputDropdown from "@/components/inputs/SearchInputDropdown";
+
+export default function SearchPage() {
   return (
-    <div className="flex justify-center">
-      <h1 className="text-5xl font-extrabold">
-        Hello world!
-      </h1>
+    <div className="p-10 relative min-h-screen w-full bg-cover bg-center bg-[url('/background.jpeg')]">
+      <div className="absolute inset-0 bg-black/50 backdrop-blur-sm z-0" />
+      <div className="relative z-10">
+        <SearchInputDropdown />
+      </div>
     </div>
   );
 }
